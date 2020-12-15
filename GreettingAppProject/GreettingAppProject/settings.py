85be224315e0decb38a9fb0c907a52fb7583b36a
Birajit <birajit95@gmail.com>
2020-12-15 22:13:21 +0530

    [Birajit] Add: Secret_key is configured

--- GreettingAppProject/GreettingAppProject/settings.py
+++ GreettingAppProject/GreettingAppProject/settings.py
@@ -16,18 +16,19 @@ import djongo
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hhug4(j8@#w9%_zgmm_cec04y^vxl)c%k@dhvv3ui18onpli6_'
+
+from .secret_settings import APP_SECRET
+
+SECRET_KEY = APP_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'GreettingAppProject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 """
@@ -80,7 +80,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }"""
-
 
 DATABASES = {
     'default': {
@@ -107,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -120,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
