d63795d6d50cb147c3712ac50a931c20ffc3655d
Birajit <birajit95@gmail.com>
2020-12-15 23:30:42 +0530

    [Birajit] Add: Database model is configured

--- GreettingAppProject/greettingApp/models.py
+++ GreettingAppProject/greettingApp/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class GreetingRecords(models.Model):
+    _id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phoneNumber = models.CharField(max_length=11)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}, {self.message[:20]}..."
