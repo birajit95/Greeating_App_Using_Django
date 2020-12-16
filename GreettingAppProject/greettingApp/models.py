0e4cbb81636d515dc1fd9230c5d86bbc932c1158
Birajit <birajit95@gmail.com>
2020-12-16 14:23:10 +0530

    [Birajit] Refactor: DataBase Model is Refactored

--- GreettingAppProject/greettingApp/models.py
+++ GreettingAppProject/greettingApp/models.py
@@ -2,10 +2,7 @@ from djongo import models
 
 
 class GreetingRecords(models.Model):
-    _id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    phoneNumber = models.CharField(max_length=11)
     message = models.TextField()
 
     def __str__(self):
