a7d3d4017d432934608910d18fc8041a420048ba
Birajit <birajit95@gmail.com>
2020-12-16 15:02:17 +0530

    [Birajit] Add: form data is saving and passing data to template

--- GreettingAppProject/greettingApp/views.py
+++ GreettingAppProject/greettingApp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render, HttpResponse
+from .models import GreetingRecords
+import json
 
 
 def home(request):
-    return render(request, "greetingApp/home.html")
+    recordData = [dict(item) for item in GreetingRecords.objects.all().values('id', 'name', 'message')]
+
+    return render(request, "greetingApp/home.html", {"data": json.dumps(recordData)})
+
+
+def addData(request):
+    if request.method == 'POST':
+        formData = json.loads(request.body.decode())
+        if formData:
+            recordData = GreetingRecords(name=formData["name"], message=formData['message'])
+            recordData.save()
+            return HttpResponse("true")
+        else:
+            return HttpResponse("false")
