79a20deda9997fe4038011260b091edd3993f633
Birajit <birajit95@gmail.com>
2020-12-17 12:29:59 +0530

    [Birajit] Add: Delete request response url is configured

--- GreettingAppProject/greettingApp/urls.py
+++ GreettingAppProject/greettingApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, addData
+from .views import home, addData, deleteRecord
 
 urlpatterns = [
 
     path('', home, name="home"),
-    path('addData/', addData, name="addData")
+    path('addData/', addData, name="addData"),
+    path('delete_record/<int: recordId>/', deleteRecord, name='deleteRecord'),
             ]
