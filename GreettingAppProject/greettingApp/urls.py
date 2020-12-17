e8632a29b3a813212fb048a6373873a0e6697a6f
Birajit <birajit95@gmail.com>
2020-12-17 18:30:46 +0530

    [Birajit] Add: record update response url is configured

--- GreettingAppProject/greettingApp/urls.py
+++ GreettingAppProject/greettingApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, addData, deleteRecord
+from .views import home, addData, deleteRecord, updateRecord
 
 urlpatterns = [
 
     path('', home, name="home"),
     path('addData/', addData, name="addData"),
     path('delete/<int:recordID>', deleteRecord, name='deleteRecord'),
+    path('update/<int:recordID>', updateRecord, name='updateRecord'),
 ]
