0062a6dfe7cfb72e3190b50ce56fbad5eba3539c
Birajit <birajit95@gmail.com>
2020-12-19 11:01:18 +0530

    [Birajit] Add: Logger is implemented

---
+++ GreettingAppProject/greettingApp/logger/logger.py
@@ -0,0 +1,4 @@
+import logging
+logging.basicConfig(filename="test.log", level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
+logger = logging.getLogger()
+
--- GreettingAppProject/greettingApp/views.py
+++ GreettingAppProject/greettingApp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from .models import GreetingRecords
+from .logger.logger import logger
 import json
 
 
 def home(request):
     recordData = GreetingRecords.objects.all()
+    logger.info("All records are displayed")
     return render(request, "greetingApp/home.html", {"data": recordData})
 
 
@@ -14,17 +16,22 @@ def addData(request):
         if formData:
             recordData = GreetingRecords(name=formData["name"], message=formData['message'])
             recordData.save()
+            logger.info(f"{recordData} Data is saved")
             data = [dict(item) for item in GreetingRecords.objects.all().values('id', 'name', 'message')]
             return HttpResponse(json.dumps(data))
         else:
+            logger.error("Data saving failed")
             return HttpResponse("false")
 
 
 def deleteRecord(request, recordID):
     if request.method == "DELETE":
-        GreetingRecords.objects.filter(id=recordID).delete()
+        record = GreetingRecords.objects.filter(id=recordID)
+        record.delete()
+        logger.info(f"{record} Record Deleted successfully")
         data = [dict(item) for item in GreetingRecords.objects.all().values('id', 'name', 'message')]
         return HttpResponse(json.dumps(data))
+    logger.error("Record Deletion failed")
     return HttpResponse("false")
 
 
@@ -36,7 +43,9 @@ def updateRecord(request, recordID):
             record.name = formData["name"]
             record.message = formData["message"]
             record.save()
+            logger.info(f"{record} Record updated successfully")
             data = [dict(item) for item in GreetingRecords.objects.all().values('id', 'name', 'message')]
             return HttpResponse(json.dumps(data))
         else:
+            logger.error("Record Updating failed")
             return HttpResponse("false")
