2c7fcdd7ca0001abe568e5d61801d891190613c4
Birajit <birajit95@gmail.com>
2020-12-19 15:00:20 +0530

    [Birajit] Refactore: BD Model is refactored

--- GreettingAppProject/greettingApp/models.py
+++ GreettingAppProject/greettingApp/models.py
@@ -4,6 +4,8 @@ from djongo import models
 class GreetingRecords(models.Model):
     name = models.CharField(max_length=100)
     message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
-        return f"{self.name}, {self.message[:20]}..."
+        return f"{self.name}, {self.message[:20]}..." if len(self.message) > 10 else f"{self.name}, {self.message}"
+    
