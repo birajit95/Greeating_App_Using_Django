9bc24d1be1aab20151890717ad50adf6b550951a
Birajit <birajit95@gmail.com>
2020-12-20 09:29:31 +0530

    [Birajit] Refactor: Model is refactored

--- GreettingAppProject/greettingApp/models.py
+++ GreettingAppProject/greettingApp/models.py
@@ -4,8 +4,10 @@ from djongo import models
 class GreetingRecords(models.Model):
     name = models.CharField(max_length=100)
     message = models.TextField()
-    date = models.DateTimeField(auto_now_add=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True, )
+
+    class Meta:
+        ordering = ['-date']
 
     def __str__(self):
         return f"{self.name}, {self.message[:20]}..." if len(self.message) > 10 else f"{self.name}, {self.message}"
-    
